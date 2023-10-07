82892a167298c241c187ee568ada5d1fc7961e68
nonexistent9 <varunraghunathan8@gmail.com>
2023-10-07 10:56:31 -0700

    added paddle class

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
 from turtle import *
 
-def move_up():
+"""def move_up():
         if right_stick.ycor()<250:
             newy = right_stick.ycor() + 10
             right_stick.goto(right_stick.xcor(),newy)
@@ -8,7 +8,7 @@ def move_up():
 def move_down():
     if right_stick.ycor() > -250:
         newy = right_stick.ycor() - 10
-        right_stick.goto(right_stick.xcor(),newy)
+        right_stick.goto(right_stick.xcor(),newy)"""
 
 screen = Screen()
 screen.bgcolor("Black")
@@ -16,22 +16,36 @@ screen.title("Pong")
 screen.setup(width=800,height=600)
 screen.tracer(0)
 
-right_stick = Turtle()
-right_stick.color("White")
-right_stick.shape("square")
-right_stick.shapesize(5,1)
-right_stick.penup()
-right_stick.goto(350,0)
+class Paddle():
+     def __init__(self, ypos, xpos):
+          self.ypos = ypos
+          self.xpos = xpos
+          stick = Turtle()
+          stick.color("White")
+          stick.shape("square")
+          stick.shapesize(5,1)
+          stick.penup()
+          stick.goto(self.ypos,self.xpos)
 
-screen.listen()
-screen.onkey(move_up,"Up")
-screen.onkey(move_down,"Down")
+     
+          
+     
+    
+
+r_paddle = Paddle(350,0)
+l_paddle = Paddle(-350,0)
+     
+
+
+
+
+
+#screen.listen()
+#screen.onkey(move_up,"Up")
+#screen.onkey(move_down,"Down")
 
 game_is_on = True
 while game_is_on:
      screen.update()
-
-
-
 
 screen.exitonclick()
